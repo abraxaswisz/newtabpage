72c912be21ef856d9023276d5e092d328178b273
bartosz <abraxaswisz@gmail.com>
2018-06-13 12:31:18 +0200

    added MoonPhases

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -7,17 +7,20 @@ class Weather extends React.Component {
   state = {
     temperature: null,
     weatherDescription: "",
-    humidity: null
+    humidity: null,
+    currentMoonPhase: "",
+    nextFullMoon: ""
   };
   componentDidMount() {
     this.renderTemperature();
+    this.renderMoonPhases();
   }
 
   renderTemperature = () => {
     const city = "Wrocław";
 
-    const requestURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${APIKEY}&units=metric`;
-    fetch(requestURL)
+    const weatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${APIKEY}&units=metric`;
+    fetch(weatherURL)
       .then(response => response.json())
       .then(response => {
         this.setState({
@@ -29,14 +32,44 @@ class Weather extends React.Component {
       })
       .catch(error => console.error(error));
   };
+  renderMoonPhases = () => {
+    const todaysDate = new Date();
+    console.log(todaysDate.getDay());
+    const todaysMonth = todaysDate.getMonth() + 1;
+    const todaysDay = todaysDate.getDate();
+    const todaysYear = todaysDate.getFullYear();
+    const moonURL = `http://api.usno.navy.mil/moon/phase?date=${todaysMonth}/${todaysDay}/${todaysYear}&nump=12`;
+    fetch(moonURL)
+      .then(response => response.json())
+      .then(response => response.phasedata)
+      .then(response => {
+        const moonPhaseArray = [...response];
+        this.setState({ currentMoonPhase: moonPhaseArray[0].phase });
+        const nextFullMoon = moonPhaseArray.find(
+          phase => phase.phase === "Full Moon"
+        );
+        return nextFullMoon;
+      })
+      .then(response => this.setState({ nextFullMoon: response.date }))
+      .catch(error => console.log(error));
+  };
 
   render() {
+    const {
+      temperature,
+      humidity,
+      weatherDescription,
+      currentMoonPhase,
+      nextFullMoon
+    } = this.state;
     return (
       <div>
-        <p>Temperatura: {this.state.temperature}</p>
-        <p>Wilgotność: {this.state.humidity}</p>
-        <p>Pogoda: {this.state.weatherDescription}</p>
-        <WeatherIcon weatherDescription={this.state.weatherDescription} />
+        <p>Temperatura: {temperature}</p>
+        <p>Wilgotność: {humidity}</p>
+        <p>Pogoda: {weatherDescription}</p>
+        <WeatherIcon weatherDescription={weatherDescription} />
+        <p>Faza księzyca: {currentMoonPhase}</p>
+        <p>Następna pełnia: {nextFullMoon}</p>
       </div>
     );
   }
