29bb007ca23f746511e9fa790750cf7d7a1da8b3
bartosz <abraxaswisz@gmail.com>
2018-06-13 13:09:09 +0200

    added basic styles

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -4,8 +4,8 @@ class SearchBar extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <form>
-          <input type="text" placeholder="Search..." />
+        <form method="get" action="https://www.google.com/search">
+          <input type="text" name="q" placeholder="Search..." />
         </form>
       </React.Fragment>
     );
