6529030badadb722833320955b71281f2e3399e9
bartosz <abraxaswisz@gmail.com>
2018-06-14 13:06:15 +0200

    converted SearchBar to a stateless component

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,15 +1,11 @@
 import React from "react";
 
-class SearchBar extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <form method="get" action="https://www.google.com/search">
-          <input type="text" name="q" placeholder="Search..." />
-        </form>
-      </React.Fragment>
-    );
-  }
+export default function SearchBar() {
+  return (
+    <React.Fragment>
+      <form method="get" action="https://www.google.com/search">
+        <input type="text" name="q" placeholder="Search..." />
+      </form>
+    </React.Fragment>
+  );
 }
-
-export default SearchBar;
