72403e5d956cce35ce8074b492cd276cb8250d6b
bartosz <abraxaswisz@gmail.com>
2018-06-15 01:07:08 +0200

    disabled autocomplete

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -4,7 +4,12 @@ export default function SearchBar() {
   return (
     <React.Fragment>
       <form method="get" action="https://www.google.com/search">
-        <input type="text" name="q" placeholder="Search..." />
+        <input
+          type="text"
+          autocomplete="off"
+          name="q"
+          placeholder="Search..."
+        />
       </form>
     </React.Fragment>
   );
