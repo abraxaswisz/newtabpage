f7c10e750c643e27285f672301990c9154275731
bartosz <abraxaswisz@gmail.com>
2018-06-14 18:44:10 +0200

    really REALLY Simplified, converted to statless component

--- src/Components/DateComponent.js
+++ src/Components/DateComponent.js
@@ -1,21 +1,5 @@
 import React from "react";
 
-export default class DateComponent extends React.Component {
-  showDate = () => {
-    let date = new Date();
-    let day = date.getDate();
-    if (day < 10) {
-      day = "0" + day;
-    }
-    let month = date.getMonth() + 1;
-    if (month < 10) {
-      month = "0" + month;
-    }
-    let year = date.getFullYear();
-    let fullDate = `${day}/${month}/${year}`;
-    return fullDate;
-  };
-  render() {
-    return <React.Fragment>{this.showDate()}</React.Fragment>;
-  }
+export default function DateComponent() {
+  return <React.Fragment>{new Date().toLocaleDateString()}</React.Fragment>;
 }
