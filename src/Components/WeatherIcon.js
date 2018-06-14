b2250e5cfd9bfe97fb0042903574c299c41627ff
bartosz <abraxaswisz@gmail.com>
2018-06-14 13:03:39 +0200

    converted to a stateless component WeatherIcon

--- src/Components/WeatherIcon.js
+++ src/Components/WeatherIcon.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { weatherIcons } from "../weatherIcons";
 
-class WeatherIcon extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <svg viewBox="0 0 30 30" width="150px" height="150px">
-          <path d={weatherIcons[this.props.weatherDescription]} />
-        </svg>
-      </React.Fragment>
-    );
-  }
+export default function WeatherIcon(props) {
+  return (
+    <React.Fragment>
+      <svg viewBox="0 0 30 30" width="150px" height="150px">
+        <path d={weatherIcons[props.weatherDescription]} />
+      </svg>
+    </React.Fragment>
+  );
 }
-
-export default WeatherIcon;
