a77a379694a1bff3cfb4222d3b5534e39f602f71
bartosz <abraxaswisz@gmail.com>
2018-06-14 13:08:44 +0200

    converted to stateless component

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,16 +3,12 @@ import "../App.css";
 import Weather from "./Weather";
 import SearchBar from "./SearchBar";
 
-class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <div>Hello</div>
-        <Weather />
-        <SearchBar />
-      </Fragment>
-    );
-  }
+export default function App() {
+  return (
+    <Fragment>
+      <div>Hello</div>
+      <Weather />
+      <SearchBar />
+    </Fragment>
+  );
 }
-
-export default App;
